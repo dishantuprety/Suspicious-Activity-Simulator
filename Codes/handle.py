8e9e62b7ee1a32817c8dd3916d69f326a7528e73
dishantuprety <dishantuprety165@gmail.com>
2025-06-01 11:33:57 +0530

    Delete Codes directory

--- Codes/handle.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-if hasattr(sys, "_MEIPASS"):
-    # Use PyInstaller's temporary folder
-    violence_model_path = os.path.join(sys._MEIPASS, "modelnew.h5")
-else:
-    # Use the normal path during development
-    violence_model_path = "modelnew.h5"
